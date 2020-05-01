ce9d0091b6bcbea1875498124cbc76713dad4e25
Nataly Makarova <natmaklis@gmail.com>
2020-05-01 17:25:34 +0300

    Updates

---
+++ precache-manifest.750db1b8318dc13aebfb8a17feb0b311.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f624f390f079edf38918a379a75d9e2d",
+    "url": "/todoList__react/index.html"
+  },
+  {
+    "revision": "cbaccfab97196f07c20d",
+    "url": "/todoList__react/static/css/main.09ccb1d6.chunk.css"
+  },
+  {
+    "revision": "8352fa37c1d058827567",
+    "url": "/todoList__react/static/js/2.c04c29e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoList__react/static/js/2.c04c29e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbaccfab97196f07c20d",
+    "url": "/todoList__react/static/js/main.e92063eb.chunk.js"
+  },
+  {
+    "revision": "00c62b9ceeb16a1c5662",
+    "url": "/todoList__react/static/js/runtime-main.30b8d539.js"
+  }
+]);
